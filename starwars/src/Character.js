e453c5a7210e07b82bfadbe4dbf8c016501a3fec
Modurotolu Olokode <durolawk@gmail.com>
2019-09-13 15:12:55 +0100

    styling components

--- starwars/src/Character.js
+++ starwars/src/Character.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const CharacterStyle = styled.div`
+    color: blue;
+    border: 2px solid black;
+    width : 20em;
+`
 
 function Character(props) {
-    const { character, height } = props;
+    const { character, height, skin_color, eye_color } = props;
     return (
         <div>
-            <div>{character}</div>
-            <div>{height}</div>
+            <CharacterStyle>
+            <h3>Warrior Info</h3>
+            <div>Name: {character}</div>
+            <div>Height: {height}</div>
+            <div>Complexion: {skin_color}</div>
+            <div>Eye Color: {eye_color}</div>
+            </ CharacterStyle>
         </div>);
 }
 
--- starwars/src/Characters.js
+++ starwars/src/Characters.js
@@ -7,7 +7,7 @@ function Characters(props) {
     <div>
         {console.log(characters
             )}
-      {characters.map((el) => <Character character={el.name} height={el.height}/>)}
+      {characters.map((el) => <Character character={el.name} height={el.height} skin_color={el.skin_color} eye_color={el.eye_color} />)}
     </div>);
 }
 
